ccda3ee0199cb8ad7515c6a8ad04b8720dd13ecb
Ellen Marie Dash <me@duckie.co>
2025-10-07 20:05:09 -0400

    Add Jim::Console.

---
+++ lib/jim/console.rb
@@ -0,0 +1,15 @@
+module Jim
+  module Console
+    def prompt(msg, default=nil, noecho: false)
+      return default unless default.nil?
+
+      print "#{msg}: "
+
+      if noecho
+        STDIN.noecho(&:gets).chomp
+      else
+        STDIN.gets.chomp
+      end
+    end
+  end
+end
