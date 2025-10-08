0a695a2b41d276ead418e68e79aeb351fe7665a5
Ellen Marie Dash <me@duckie.co>
2025-10-07 20:06:02 -0400

    Add "jim signout"; add "--host", "--user", and "--otp" flags to "jim signin".

--- lib/jim/cli.rb
+++ lib/jim/cli.rb
@@ -1,9 +1,13 @@
 require_relative "client"
 require_relative "config"
+require_relative "console"
+require_relative "simple_opts"
 
 module Jim
   module Cli
-    METHODS = %w[gemspec signin help]
+    extend Jim::Console
+
+    METHODS = %w[gemspec signin signout help]
 
     def self.run
       if METHODS.include?(ARGV[0])
@@ -16,28 +20,41 @@ module Jim
     def self.config(command, setting, value=nil)
       config = Jim::Config.load_or_create
 
-
-
       Jim::Config.set(setting, value) unless value.nil?
 
       Jim::Config.get(setting)
     end
 
     # Sign in to the specified gem server.
-    def self.signin(args=nil)
+    def self.signin(*args)
       require "io/console"
 
-      gem_host = "https://rubygems.org"
+      opts = SimpleOpts.new(
+        defaults: { host: "https://rubygems.org" },
+        banner: "Usage: jim signin [--host HOST] [--otp CODE]",
+      )
 
-      print "Username: "
-      username = STDIN.gets.chomp
+      opts.simple("--host HOST",
+                  "Address of gem host to push to.",
+                  :host)
 
-      print "Password: "
-      password = STDIN.noecho(&:gets).chomp
+      opts.simple("--user USERNAME",
+                  "Username to sign in as.",
+                  :user)
+
+      opts.simple("--otp CODE",
+                  "Multifactor authentication code.",
+                  :otp)
+
+      options = opts.parse_and_consume_all!(args)
+
+      gem_host = prompt("Host", options[:host])
+
+      username = prompt("Username", options[:user])
+      password = prompt("Password", noecho: true)
       puts
 
-      print" OTP: "
-      otp = STDIN.gets.chomp
+      otp = prompt("OTP", options[:otp])
 
       config = Jim::Client.new(gem_host).sign_in(username, password, otp)
       name = config["name"]
@@ -45,6 +62,11 @@ module Jim
 
       puts "Saved key with name \"#{name}\" and scopes:"
       puts "- " + scopes.filter {|k, v| v}.keys.map(&:to_s).join("\n- ")
+    end
+
+    # Sign out from configured gem host
+    def self.signout
+      Jim::Config.delete_api_key
     end
 
     # Print information about the gemspec in the current directory.
