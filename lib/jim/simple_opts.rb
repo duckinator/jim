dc2e7573b56138212bf7b0f053ab577294e65467
Ellen Marie Dash <me@duckie.co>
2025-10-07 20:04:57 -0400

    add Jim::SimpleOpts.

---
+++ lib/jim/simple_opts.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# This file was based off okay/simple_opts.
+
+require "optparse"
+
+module Jim
+  ##
+  # An OptionParser wrapper providing a few convenience functions.
+  class SimpleOpts < OptionParser
+    class SimpleOptsError < StandardError; end
+    class UnexpectedArgument < SimpleOptsError; end
+
+    def initialize(*args, banner: nil, defaults: nil)
+      super(*args)
+      @okay_options = defaults || {}
+
+      self.banner = banner unless banner.nil?
+
+      separator "\nOptions:"
+    end
+
+    # simple(..., :a)
+    # simple(..., :b)
+    #   ==
+    # options = {}
+    # on(...) { |val| options[:a] = val }
+    # on(...) { |val| options[:b] = val }
+    def simple(*args)
+      key = args.pop
+      on(*args) { |*x| @okay_options[key] = x[0] }
+    end
+
+    def parse(args)
+      parse!(args.dup)
+      @okay_options
+    end
+
+    def parse_and_consume_all!(args)
+      args = args.dup
+      parse!(args)
+      raise UnexpectedArgument, args[0] unless args.empty?
+      @okay_options
+    end
+  end
+end
+
