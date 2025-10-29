6b954a79d120ba17417127fc16951cd18fe67e96
Ellen Marie Dash <me@duckie.co>
2025-10-29 16:38:56 -0400

    Make most of Packer an fixed string.

--- lib/jim/packer.rb
+++ lib/jim/packer.rb
@@ -2,6 +2,63 @@ require "json"
 
 module Jim
   module Packer
+    UNPACKER = <<~EOF
+      require 'json'
+      require 'pathname'
+
+      $JIM_DATA = JSON.load(DATA)
+      $PACKED_BY_JIM = $JIM_DATA["name"]
+
+      $JIM_DEBUG = ENV['JIM_DEBUG']
+
+      module Kernel
+        def jim_require_relative(path, relative_to)
+          base_dir = Pathname(relative_to).dirname
+          full_path = Pathname(base_dir).join(path).sub_ext(".rb").to_s
+
+          if $JIM_DATA["files"].keys.include?(full_path)
+            puts "Loading packed " + full_path + "..." if $JIM_DEBUG
+            unless $JIM_LOADED.include?(full_path)
+              eval($JIM_DATA["files"][full_path], binding, full_path, 0)
+              $JIM_LOADED.push(full_path)
+            end
+            return true
+          end
+
+          raise LoadError, "cannot load such file -- " + full_path + " (" + path + ")"
+        end
+
+        alias_method :jim_orig_require, :require
+        def require(path)
+          jim_orig_require(path)
+        rescue LoadError => e
+          raise e unless e.path == path
+
+          $JIM_DATA["require_paths"].each do |req_path|
+            combined = Pathname(req_path).join(path).sub_ext(".rb").to_s
+            $JIM_LOADED ||= []
+            if $JIM_DATA["files"].keys.include?(combined)
+              puts "Loading packed " + combined + "..." if $JIM_DEBUG
+              unless $JIM_LOADED.include?(combined)
+                eval($JIM_DATA["files"][combined], binding, combined, 0)
+                $JIM_LOADED.push(combined)
+              end
+              return true
+            end
+          end
+
+          raise e
+        end
+      end
+
+      $JIM_EXECUTABLE = $JIM_DATA["executable"]
+      $JIM_EXE_CONTENTS = $JIM_DATA["files"][$JIM_EXECUTABLE]
+      eval($JIM_EXE_CONTENTS, binding, $JIM_EXECUTABLE, 0)
+
+      __END__
+    EOF
+
+
     def self.pack(spec)
       files = spec.files.map { |file|
         contents = File.read(file)
@@ -10,72 +67,20 @@ module Jim
       }.to_h
 
       data = {
+        "name": spec.name,
         "files": files,
         "require_paths": spec.require_paths,
         "executable": File.join(spec.bindir, spec.executables.first)
       }
 
-      <<~EOF
-        #!/usr/bin/env ruby
-
-        # #{spec.name} #{spec.version} 0.2.0
-        # Packed by Jim #{Jim::VERSION}
-
-        require 'json'
-        require 'pathname'
-
-        $PACKED_BY_JIM = #{spec.name.inspect}
-        $JIM_DATA = JSON.load(DATA)
-
-        $JIM_DEBUG = ENV['JIM_DEBUG']
-
-        module Kernel
-          def jim_require_relative(path, relative_to)
-            base_dir = Pathname(relative_to).dirname
-            full_path = Pathname(base_dir).join(path).sub_ext(".rb").to_s
-
-            if $JIM_DATA["files"].keys.include?(full_path)
-              puts "Loading packed " + full_path + "..." if $JIM_DEBUG
-              unless $JIM_LOADED.include?(full_path)
-                eval($JIM_DATA["files"][full_path], binding, full_path, 0)
-                $JIM_LOADED.push(full_path)
-              end
-              return true
-            end
-
-            raise LoadError, "cannot load such file -- " + full_path + " (" + path + ")"
-          end
-
-          alias_method :jim_orig_require, :require
-          def require(path)
-            jim_orig_require(path)
-          rescue LoadError => e
-            raise e unless e.path == path
-
-            $JIM_DATA["require_paths"].each do |req_path|
-              combined = Pathname(req_path).join(path).sub_ext(".rb").to_s
-              $JIM_LOADED ||= []
-              if $JIM_DATA["files"].keys.include?(combined)
-                puts "Loading packed " + combined + "..." if $JIM_DEBUG
-                unless $JIM_LOADED.include?(combined)
-                  eval($JIM_DATA["files"][combined], binding, combined, 0)
-                  $JIM_LOADED.push(combined)
-                end
-                return true
-              end
-            end
-
-            raise e
-          end
-        end
-
-        $JIM_EXECUTABLE = $JIM_DATA["executable"]
-        $JIM_EXE_CONTENTS = $JIM_DATA["files"][$JIM_EXECUTABLE]
-        eval($JIM_EXE_CONTENTS, binding, $JIM_EXECUTABLE, 0)
-
-        __END__
-        #{JSON.dump(data)}
-      EOF
+      [
+        "#!/usr/bin/env ruby",
+        "",
+        "# #{spec.name} #{spec.version}",
+        "# Packed by Jim #{Jim::VERSION}",
+        UNPACKER,
+        JSON.dump(data),
+      ].join("\n")
     end
   end
 end
